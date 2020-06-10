ab09071ec00072da81ed360b22caff05808bf28a
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-06-10 08:53:29 +0530

    Completed Forensics Dashboard

--- src/pages/HomeFo.js
+++ src/pages/HomeFo.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class App extends Component {
-    state = {};
+    state = {
+        redirect: "",
+    };
+
+    logout = () => {
+        localStorage.removeItem("session");
+        localStorage.removeItem("user");
+        this.setState({ redirect: <Redirect to="/" /> });
+    };
 
     render() {
-        return <div>Hi There</div>;
+        return (
+            <div>
+                <h2>Forensics Dashboard</h2>
+                <h2>
+                    {this.state.redirect}Welcome, {localStorage.getItem("user")}!
+                </h2>
+                <Link to="/submitEvidence">Add Evidence</Link> <br />
+                <Link to="/evidenceViewer">View Evidence</Link> <br />
+                <Link to="/viewInvestigation/0">View Investigation</Link> <br />
+                <Button m={1} onClick={this.logout} variant="contained" color="primary">
+                    Log Out
+                </Button>
+            </div>
+        );
     }
 }
 
